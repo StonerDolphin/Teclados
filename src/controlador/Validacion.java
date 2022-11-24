f92491c5207dd1e8a7fb12cd0dd47dff9f671950
StonerDolphin <cam.trivino@gmail.com>
2022-11-24 20:21:43 -0300

    metodos de validacion

--- src/controlador/Validacion.java
+++ src/controlador/Validacion.java
@@ -4,10 +4,40 @@
  */
 package controlador;
 
-/**
- *
- * @author trivi
- */
 public class Validacion {
+    
+    public boolean validaVacio(String texto)
+    {
+        return !texto.isEmpty();
+    }
+    public boolean validaVacio2(String texto)
+    {
+        if (texto.length()==0) {
+            return false;
+        }else
+        {
+            return true;
+        }
+    }
+    public boolean validaInt(String texto)
+    {
+        try
+        {
+            Integer.parseInt(texto);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+    public boolean validaCombo(String texto)
+    {
+        if (texto.equals("Seleccionar")) {
+            return false;
+        }else
+        {
+            return true;
+        }
+    }
     
 }
