53915644688dd189833b9535f9cf4eb1cca9f044
StonerDolphin <cam.trivino@gmail.com>
2022-11-25 21:24:59 -0300

    dispoce

--- src/Vista/Agregar.java
+++ src/Vista/Agregar.java
@@ -240,7 +240,7 @@ public class Agregar extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        System.exit(0);
+        dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
